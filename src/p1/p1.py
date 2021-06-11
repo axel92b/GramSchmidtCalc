41a9d7b958ff5659cd006efb1d42bd1e0575fb16
Vladimir-pa <vov4ikpa@gmail.com>
2021-06-11 12:21:56 +0300

    finished p2 and almost p3

--- src/p1/p1.py
+++ src/p1/p1.py
@@ -1,44 +1,37 @@
 import numpy as np
 import numpy.linalg
 
+class GramSchmidtCalc:
 
-def get_matrix() -> np.ndarray:
-    return np.array([
-        [0, 3, 4],
-        [1, 0, 1],
-        [1, 1, 3]
-    ])
+    def __projection(self, a: np.array, b: np.array) -> np.array:
+        return (np.dot(a, b) / np.dot(b, b)) * b
 
+    def __get_vec_normal(self, a: np.array) -> float:
+        return np.dot(a, a) ** 0.5
 
-def projection(a: np.array, b: np.array) -> np.array:
-    return (np.dot(a, b) / np.dot(b, b)) * b
+    def __normalize_vec(self, a: np.array) -> np.array:
+        vec_normal = self.__get_vec_normal(a)
+        for i, val in enumerate(a):
+            a[i] = val / vec_normal
 
-
-def get_vec_normal(a: np.array) -> float:
-    return np.dot(a, a) ** 0.5
-
-
-def normalize_vec(a: np.array) -> np.array:
-    vec_normal = get_vec_normal(a)
-    for i, val in enumerate(a):
-        a[i] = val / vec_normal
-
-
-def gram_schmidt_calc(a: np.ndarray) -> np.ndarray:
-    b: np.ndarray = a.copy()[:].astype(numpy.float32)
-    for i in range(0, b.shape[0] - 1):
-        for j in range(i + 1, b.shape[0]):
-            b[j] -= projection(b[j], b[i])
-        normalize_vec(b[i])
-    normalize_vec(b[-1])
-    return b
+    def calc(self, a: np.ndarray) -> np.ndarray:
+        if numpy.linalg.matrix_rank(a) != a.shape[0]:
+            raise Exception("Linearly dependant vectors are inside")
+        b: np.ndarray = a.copy()[:].astype(numpy.float32)
+        for i in range(0, b.shape[0] - 1):
+            for j in range(i + 1, b.shape[0]):
+                b[j] -= self.__projection(b[j], b[i])
+            self.__normalize_vec(b[i])
+        self.__normalize_vec(b[-1])
+        return b
 
 
 def main():
-    matrix = get_matrix()
-    if numpy.linalg.matrix_rank(matrix) != matrix.shape[0]:
-        raise Exception("Linearly dependant vectors are inside")
-    print(gram_schmidt_calc(matrix))
+    matrix = np.array([[0,3,4],
+                       [1,0,1],
+                       [1,1,3]])
+    gsc = GramSchmidtCalc()
+    print(gsc.calc(matrix))
 
 
 if __name__ == '__main__':
--- src/p2/p2.py
+++ src/p2/p2.py
@@ -1,1 +1,62 @@
 import numpy as np
+import numpy.linalg
+
+
+class GramSchmidtCalc:
+    inner_product_matrix: np.ndarray
+
+    def __init__(self, inner_product_matrix: np.ndarray) -> None:
+        if self.__check__if_matrix_is_absolutely_positive(inner_product_matrix) == False:
+            raise Exception("The provided matrix is not absolutely positive!")
+        self.inner_product_matrix = inner_product_matrix
+
+    def inner_product(self, a: np.array, b: np.array):
+        return np.matmul(np.matmul(a, self.inner_product_matrix), b)
+
+    def __projection(self, a: np.array, b: np.array) -> np.array:
+
+        return (self.inner_product(a, b) / self.inner_product(b, b)) * b
+
+    def __get_vec_normal(self, a: np.array) -> float:
+        return self.inner_product(a, a) ** 0.5
+
+    def __normalize_vec(self, a: np.array) -> np.array:
+        vec_normal = self.__get_vec_normal(a)
+        for i, val in enumerate(a):
+            a[i] = val / vec_normal
+
+    def calc(self, matrix: np.ndarray) -> np.ndarray:
+        if self.is_matrix_linearly_dependent(matrix):
+            raise Exception("Error: Matrix is linearly dependent")
+        res: np.ndarray = matrix.copy()[:].astype(numpy.float32)
+        for i in range(0, res.shape[0] - 1):
+            for j in range(i + 1, res.shape[0]):
+                res[j] -= self.__projection(res[j], res[i])
+            self.__normalize_vec(res[i])
+        self.__normalize_vec(res[-1])
+        return res
+
+    @staticmethod
+    def is_matrix_linearly_dependent(a):
+        return numpy.linalg.matrix_rank(a) != a.shape[0]
+
+    @staticmethod
+    def __check__if_matrix_is_absolutely_positive(inner_product_matrix):
+        return np.all(np.linalg.eigvals(inner_product_matrix) > 0)
+
+
+
+
+def main():
+    matrix = np.array([[0, 3, 4],
+                       [1, 0, 1],
+                       [1, 1, 3]])
+    gsc = GramSchmidtCalc(np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]]))
+    result = gsc.calc(matrix)
+    if not GramSchmidtCalc.is_matrix_linearly_dependent(result):
+        print("The result is an ortonormal basis for the field")
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
